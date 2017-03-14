b09ab865ef1974059ea0142497297ddf1dfc81ec
Phillip Jenks <phillip.jenks@gmail.com>
2017-03-14 16:37:00 -0400

    Added UI widgets and controls

---
+++ work_log/work_log/CLogger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace work_log {
+
+    enum WorkLogState {
+        STATE_IDLE,
+        STATE_TIMING,
+        STATE_FINISHED,
+
+        STATE_COUNT
+    };
+
+    class CLogger {
+
+        private static String k_TimeFormat = "g";
+        private static CultureInfo k_Culture = CultureInfo.CreateSpecificCulture("en-us");
+
+        private WorkLogState m_state = WorkLogState.STATE_IDLE;
+        public WorkLogState State {
+            get { return m_state; }
+        }
+
+        private DateTime m_startTime = new DateTime();
+        public String StartTime {
+            get {
+                return m_state == WorkLogState.STATE_TIMING || m_state == WorkLogState.STATE_FINISHED ? m_startTime.ToString(k_TimeFormat, k_Culture) : "N/A";
+            }
+        }
+
+        private DateTime m_endTime = new DateTime();
+        public String EndTime {
+            get {
+                return m_state == WorkLogState.STATE_FINISHED ? m_endTime.ToString(k_TimeFormat, k_Culture) : "N/A";
+            }
+        }
+
+        private String m_logMsg;
+        public String LogMessage {
+            get {
+                return m_logMsg;
+            }
+        }
+
+        public CLogger() {
+        }
+
+        public void Init() {
+        }
+
+        public void AdvanceState() {
+            switch (m_state) {
+                case WorkLogState.STATE_IDLE:
+                case WorkLogState.STATE_FINISHED:
+                    m_state = WorkLogState.STATE_TIMING;
+                    StartTiming();
+                    break;
+                case WorkLogState.STATE_TIMING:
+                    m_state = WorkLogState.STATE_FINISHED;
+                    StopTiming();
+                    break;
+            }
+        }
+
+        public void SetMessage(String msg) {
+            m_logMsg = msg;
+        }
+
+        private void StartTiming() {
+            m_startTime = DateTime.Now;
+        }
+
+        private void StopTiming() {
+            m_endTime = DateTime.Now;
+        }
+    }
+}
---
+++ work_log/work_log/work_log.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace work_log {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window {
+
+        private CLogger m_logger = new CLogger();
+
+        public MainWindow() {
+            InitializeComponent();
+
+            m_logger.Init();
+
+            HandleStateChange(); 
+        }
+
+        private void start_stop_button_Click(object sender, RoutedEventArgs e) {
+            HandleButtonClick();
+        }
+
+        private void HandleButtonClick() {
+            switch(m_logger.State) {
+                case WorkLogState.STATE_IDLE:
+                case WorkLogState.STATE_FINISHED:
+                    m_logger.AdvanceState();
+                    HandleStateChange();
+                    break;
+                case WorkLogState.STATE_TIMING: 
+                    {
+                        if(TryGetMessage()) {
+                            m_logger.AdvanceState();
+                            HandleStateChange();
+                        } else {
+                            MessageBox.Show("You must enter a log message!");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void HandleStateChange() {
+            switch (m_logger.State) {
+                case WorkLogState.STATE_IDLE:
+                    HandleIdle();
+                    break;
+                case WorkLogState.STATE_TIMING:
+                    HandleTiming();
+                    break;
+                case WorkLogState.STATE_FINISHED:
+                    HandleFinished();
+                    break;
+            }
+        }
+
+        private bool TryGetMessage() {
+
+            String msg = msg_txt.Text.Trim(' ');
+            if(msg.Length <= 0) {
+                return false;
+            }
+
+            m_logger.SetMessage(msg);
+
+            return true;
+        }
+
+        private void HandleIdle() {
+            main_label.Content = "Click The Start Button To Start A Session";
+            start_stop_button.Content = "Start";
+            msg_txt.Visibility = System.Windows.Visibility.Hidden;
+            log_msg.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void HandleTiming() {
+            main_label.Content = "Started Timing " + m_logger.StartTime;
+            start_stop_button.Content = "Stop";
+            msg_txt.Visibility = System.Windows.Visibility.Visible;
+            log_msg.Visibility = System.Windows.Visibility.Visible;
+            log_msg.Content = "Enter Log Message";
+        }
+        private void HandleFinished() {
+            main_label.Content = "Started: " + m_logger.StartTime + "\tFinished: " + m_logger.EndTime;
+            start_stop_button.Content = "Start";
+            log_msg.Content = m_logger.LogMessage;
+            msg_txt.Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
